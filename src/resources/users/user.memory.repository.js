6ea8890fc8024f92a0d6d83b519298769ceed756
AliakseiMatsuk <matsukaliaksei@gmail.com>
2020-10-08 13:37:35 +0300

    Endpoints for Users added

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,36 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const DB = require('../../utils/inMemoryDb');
+const NOT_FOUND_ERROR = require('../../errors/appError');
+
+const TABLE_NAME = 'Users';
+
+const getAll = async () => DB.getAllEntities(TABLE_NAME);
+
+const get = async id => {
+  const user = await DB.getEntity(TABLE_NAME, id);
+
+  if (!user) {
+    throw new NOT_FOUND_ERROR(`Couldn't find a user with id: ${id}`);
+  }
+
+  return user;
 };
 
-module.exports = { getAll };
+const remove = async id => {
+  if (!(await DB.removeEntity(TABLE_NAME, id))) {
+    throw new NOT_FOUND_ERROR(`Couldn't find a user with id: ${id}`);
+  }
+};
+
+const save = async user => await DB.saveEntity(TABLE_NAME, user);
+
+const update = async (id, user) => {
+  const updatedUser = await DB.updateEntity(TABLE_NAME, id, user);
+
+  if (!updatedUser) {
+    throw new NOT_FOUND_ERROR(`Couldn't find a user with id: ${id}`);
+  }
+
+  return updatedUser;
+};
+
+module.exports = { getAll, get, remove, save, update };
